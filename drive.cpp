ebb2b5be6f1acaaf6ee886f13a0aa7ea423c345b
xavenna <adam.evanson@gmail.com>
2022-06-20 20:26:44 -0400

    Improved drive packing, fixed padding

--- drive.cpp
+++ drive.cpp
@@ -33,27 +33,29 @@ bool packFolderToDrive(std::string folderName, std::string driveName) {
     temp = entry.path().filename().string();
     //temp is the file to be packed.
     //determine file size
-    auto size = fs::file_size(entry.path());
-    int sectors = ceil(size / 1024);
-    fileTable += (temp.substr(0, temp.find(".")).substr(0,8) + "." + temp.substr(temp.find(".")).substr(0,3));
+    long size = fs::file_size(entry.path());
+    int sectors = ceil((float) size / 1024);
+    fileTable += padStringToSize(temp.substr(0, temp.find(".")).substr(0,8) + "." + temp.substr(temp.find(".")+1).substr(0,3), 12);
     //add file name to table
     fileTable += padTo2Bytes(currentSector);
     //determine next sector
     currentSector += sectors;
-    fileTable += padTo2Bytes(currentSector);
+    fileTable += padTo2Bytes(currentSector-1); //this won't work for empty files, make sure this doesn't break things
 
     get.open(temp, std::ios::binary);
     std::filebuf* pbuf = get.rdbuf();
     char* buffer=new char[size];
     pbuf->sgetn(buffer, size);
     get.close();
+
     //pad buffer 
     std::string fileContent(buffer);
-    padStringToSize(fileContent, sectors * 1024);
-    driveBody += fileContent;
+    
+    driveBody += padStringToSize(fileContent, sectors * 1024);
     delete[] buffer;
   }
   //write filetable to file
+  fileTable = padStringToSize(fileTable, 1024); //pad filetable (eventually add size check)
   drive.write(fileTable.c_str(), fileTable.size());
   drive.write(driveBody.c_str(), driveBody.size());
 
